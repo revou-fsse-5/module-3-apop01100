d86f1892848dd9de913d19b85bb2d954ad64138e
Thoriq Afif <mthoriqafif01@gmail.com>
2024-07-27 02:18:39 +0700

    Change height body to 100% when displaying recipe and add some comments to syntax

--- src/main.js
+++ src/main.js
@@ -1,34 +1,45 @@
-const recipeContainer = document.querySelector('.recipe-container')
-const recipeRandomButton = document.querySelector('#getRecipeBtn')
+// Makes variable for access elements in index.html file
+const body = document.querySelector('body');
+const recipeRandomButton = document.querySelector('#getRecipeBtn');
 const recipeTitle = document.querySelector('.recipe-title');
-const recipeImage = document.querySelector('img');
+const recipeImage = document.querySelector('.recipe-image');
 const recipeInstructions = document.querySelector('.recipe-instructions');
 
+// Setup size values when display the recipe contents
+let widthImage = '200px';
+let bodyHeight = '100%';
+
+// Fetch function for takes data from API random recipe (https://www.themealdb.com/api/json/v1/1/random.php)
 const fetchData = async function () {
+    // API URL
     const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
     try {
-        const response = await fetch(url,
-            {cache: 'no-cache',
-             mode: 'cors',
-             redirect: 'follow',
-                                        
-        });
+        const response = await fetch(url); // fetch process
 
+        // Condition for response doesn't fetch the data, will throw the Error
         if (!response.ok) {
             throw new Error (`Response status: ${response.status}`);
         }
+
         const json = await response.json()
-        return json;
+        return json; // object return
     } catch (error) {
         console.error(error.message);
     }
 }
 
 recipeRandomButton.onclick = async function () {
-    const data = await fetchData();
-    recipeTitle.innerHTML = data.meals[0].strMeal;
-    recipeImage.src = data.meals[0].strMealThumb;
-    recipeImage.style.width = "200px"
-    recipeInstructions.innerHTML = data.meals[0].strInstructions;
+    const data = await fetchData(); // fetch the data and then assign object data to variable
+
+    let foodName = data.meals[0].strMeal; // take food value name from object
+    let foodImage = data.meals[0].strMealThumb; // take image URL from object
+    let foodRecipe = data.meals[0].strInstructions; // take recipe instructions from object
+
+    body.style.height = bodyHeight; // Change height body to 100%
+    recipeTitle.innerHTML = foodName; // Set food name to inner h2 element
+    recipeImage.src = foodImage; // Set image food with URL to src img element
+    recipeImage.alt = foodImage; // Set food name to alt img element
+    recipeImage.style.width = widthImage; // Set width img element
+    recipeInstructions.innerHTML = foodRecipe; // Set recipe instructions to inner p element
 }
